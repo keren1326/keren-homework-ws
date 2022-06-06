26e737fa3d2c8c511656429d2ccc1fb6f9f29ada
keren1326 <kerenbouzaglo@gmail.com>
2022-06-07 00:25:09 +0300

    suite timeout

--- code/timeout/js/main.js
+++ code/timeout/js/main.js
@@ -2,12 +2,13 @@ var main=document.querySelector('main');
 var btn=document.querySelector("button");
 btn.addEventListener( "click", getValue);
 btn.addEventListener("click", zeroToNine );
-
+var clickedNum=[];
 var allInputs=[];
-
+var count=0;
 function getValue(){
     var input=document.getElementById("num").value;
   allInputs.push(input);
+
    console.log(allInputs);
    main.innerHTML="";
 
@@ -29,45 +30,98 @@ var clavier = `
 </div>`;
 
 main.innerHTML+= clavier;
-var clickedNum=[];
- var zero =document.querySelector( "#zero").addEventListener('click', function(){clickedNum.push("0");console.log(clickedNum);});
- var one =document.querySelector("#one").addEventListener('click', function(){clickedNum.push("1");console.log(clickedNum);});
- var two =document.querySelector("#two").addEventListener('click', function(){clickedNum.push("2");console.log(clickedNum);});
- var three =document.querySelector( "#three").addEventListener('click', function(){clickedNum.push("3");console.log(clickedNum);});
- var four =document.querySelector( "#four").addEventListener('click', function(){clickedNum.push("4");console.log(clickedNum);});
- var five =document.querySelector( "#five").addEventListener('click', function(){clickedNum.push("5");console.log(clickedNum);});
- var six =document.querySelector( "#six").addEventListener('click', function(){clickedNum.push("6");console.log(clickedNum);});
- var seven =document.querySelector( "#seven").addEventListener('click', function(){clickedNum.push("7");console.log(clickedNum);});
- var eight =document.querySelector("#eight").addEventListener('click', function(){clickedNum.push("8");console.log(clickedNum);});
- var nine =document.querySelector( "#nine").addEventListener('click', function(){clickedNum.push("9");console.log(clickedNum);});
-// do {
-//   for (let i = 0; i < clickedNum.length; i++) {
-//     const element = clickedNum[i];
-//     if (element==allInputs[i]) {
-//       alert("bon code")
-//     }
-//   }
-// } while (clickedNum>=4);
-     
-   
+var buttons=document.getElementsByTagName("button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", ColorCode)
+  buttons[i].addEventListener("click", verif);
 }
- 
 
-// Définissez un code à 4 chiffres à l'aide d'un input et d'un bouton. Une fois le 
-// bouton cliqué, remplacez l'écran par un clavier contenant des chiffres de 0 à 
-// 9 SEULEMENT! 
-// La vérification de correspondance de code est appelée une fois que vous 
-// avez appuyé sur quatre boutons. 
-// Vous avez trois tentatives pour entrer le bon code. S'il est entré 
-// correctement, alertez un message de "Success", sinon, un message d'erreur. 
-// Après trois tentatives, alertez "La police arrive!". 
-// Chaque fois que vous appuyez sur les boutons, elles changent de couleur. 
-// TOUS les quatre numéros doivent être colorés AVANT que l'alerte ne soit 
-// affichée. 
-// Une fois la boîte de dialogue d'alerte fermée, les couleurs reviennent à leur 
-// état d'origine. 
-// S'il y a un délai de trois secondes entre les touches, les touches reprennent 
-// leur couleur d'origine et la séquence est annulée. Mais, cette séquence 
-// n'est pas comptée comme un essai. 
-// Les fonctions ne doivent pas dépasser 10 lignes de code APRÈS 
-// l’embellissement. Cela inclut les crochets de fermeture.
+//  var zero =document.querySelector( "#zero")
+//  zero.addEventListener('click', verif);
+//  var one =document.querySelector("#one")
+// // one.addEventListener('click',verif);
+//  var two =document.querySelector("#two")
+// // two.addEventListener('click',verif);
+//  var three =document.querySelector( "#three")
+//  three.addEventListener('click',verif );
+//  var four =document.querySelector( "#four")
+//  four.addEventListener('click',verif);
+//  var five =document.querySelector( "#five")
+//  five.addEventListener('click',verif);
+//  var six =document.querySelector( "#six")
+//  six.addEventListener('click',verif);
+//  var seven =document.querySelector( "#seven")
+//  seven.addEventListener('click', verif);
+//  var eight =document.querySelector("#eight")
+//  eight.addEventListener('click', verif);
+//  var nine =document.querySelector( "#nine")
+//  nine.addEventListener('click', verif);
+
+
+}
+function ColorCode() {
+  var makingColorCode = '0123456789ABCDEF';
+  var finalCode = '#';
+  for (var counter = 0; counter < 6; counter++) {
+     finalCode =finalCode+ makingColorCode[Math.floor(Math.random() * 16)];
+  }
+  //return finalCode;
+  this.style.background = finalCode;
+ 
+}
+//Function calling on button click.
+// function getRandomColor() {
+//   $(this).css("background-color", ColorCode());
+// }
+
+function verif(){
+  var z=this.value;
+
+   if(z=="0"){
+     clickedNum.push(zero.value);
+   }
+   else if(z=="1"){
+     clickedNum.push("1");
+   }
+   else if(z=="2"){
+     clickedNum.push("2");
+   }
+   else if(z=="3"){
+     clickedNum.push("3");
+   }
+   else if(z=="4"){
+     clickedNum.push("4");
+   }
+   else if(z=="5"){
+     clickedNum.push("5");
+   }
+   else if(z=="6"){
+     clickedNum.push("6");
+   }
+   else if(z=="7"){
+     clickedNum.push("7");
+    }
+   else if(z=="8"){
+     clickedNum.push("8");
+   }
+   else if(z=="9"){
+     clickedNum.push("9");
+   }
+
+  console.log(clickedNum);
+   if(clickedNum.length==4 ||clickedNum.length==8||clickedNum.length==12){match()}
+   if(count==3){alert(" la police arrive")} 
+}
+
+function match(){
+
+var allNum=  clickedNum.join("");
+  if(allNum==allInputs){
+    setTimeout(alert , 100 ,'sucess') ;
+  
+   }
+ else{
+  count++; 
+  setTimeout(alert , 100 ,'try again') 
+  
+}}
